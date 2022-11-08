9e3163da3fc37963e7774375ea47de0214e19da3
Amy Galbicsek <acgalbicsek@gmail.com>
2022-11-08 13:35:29 -0900

    finished exercise

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+string chickenName = "Goldie";
+int age = 2;
+char middleInitial = 'H';
+bool isCurrentlyLaying = true;
+double currentTemp =25;
+decimal currentPrice =6.50m;
+
+Console.WriteLine($"Chicken Name: {chickenName}");
+Console.WriteLine($"age: {age}");
+Console.WriteLine($"middle Initial: {middleInitial}");
+Console.WriteLine($"Is Currently Laying: {isCurrentlyLaying}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+                    
